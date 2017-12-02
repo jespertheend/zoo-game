9ac833c03118d5b4051343aa54088675e5f97668
Jesper van den Ende <jespertheend@gmail.com>
2017-12-02 10:28:17 +0100

    Add walking system in overworld

---
+++ Zoo Game Unity/Assets/Scripts/Generic/SetGravity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGravity : MonoBehaviour {
+
+	public Vector3 gravity = new Vector3(0f, -9.81f, 0f);
+	public Vector2 gravity2D = new Vector3(0f, -9.81f);
+
+	void Start () {
+		Physics.gravity = gravity;
+		Physics2D.gravity = gravity2D;
+	}
+}
---
+++ Zoo Game Unity/Assets/Scripts/Overworld/OverworldCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldCharacter : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	public float maxWalkForce = 1f;
+	public float walkForceMultiplier = 1f;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		force *= walkForceMultiplier;
+		force = Vector2.ClampMagnitude(force, maxWalkForce);
+		rb.AddForce(force);
+	}
+}
