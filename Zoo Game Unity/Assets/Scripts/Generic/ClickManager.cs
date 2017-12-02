2839322a2a322b7a6e2f73230b877ec6197771ac
Jesper van den Ende <jespertheend@gmail.com>
2017-12-02 22:01:28 +0100

    better click scripts

--- Zoo Game Unity/Assets/Scripts/Generic/ClickManager.cs
+++ Zoo Game Unity/Assets/Scripts/Generic/ClickManager.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour {
 
+	GenericClickableObject prevClickScript;
+
 	void Update () {
-		if(Input.GetMouseButtonDown(0)){
+		GenericClickableObject clickScript = null;
+		if(Input.GetMouseButton(0)){
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
 			if(hit.collider != null){
-				GenericClickableObject clickScript = hit.collider.gameObject.GetComponent<GenericClickableObject>();
-				if(clickScript != null){
-					clickScript.OnClick();
-				}
+				clickScript = hit.collider.gameObject.GetComponent<GenericClickableObject>();
+			}
+		}
+		if(clickScript != prevClickScript){
+			if(prevClickScript != null){
+				prevClickScript.FireUnClick();
+				prevClickScript = null;
+			}
+			if(clickScript != null){
+				clickScript.FireClick();
+				prevClickScript = clickScript;
 			}
 		}
 	}
--- Zoo Game Unity/Assets/Scripts/Generic/GenericClickableObject.cs
+++ Zoo Game Unity/Assets/Scripts/Generic/GenericClickableObject.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class GenericClickableObject : MonoBehaviour {
 
+	public bool isClicking {get; private set;}
 	public virtual void OnClick(){}
+	public virtual void OnRelease(){}
+
+	public void FireClick(){
+		isClicking = true;
+		OnClick();
+	}
+
+	public void FireUnClick(){
+		isClicking = false;
+		OnRelease();
+	}
 }
