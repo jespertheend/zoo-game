7df63658c0e194e6dace122a1c8e40d825c79012
Jesper van den Ende <jespertheend@gmail.com>
2017-12-02 15:33:22 +0100

    add initial task list UI

---
+++ Zoo Game Unity/Assets/Scripts/Gameplay/Tasks/TaskListUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskListUI : MonoBehaviour {
+
+	public RectTransform listRect;
+	public float animationSpeed = 1f;
+
+	float animationTime = 0f;
+
+	void Update(){
+		if(Input.GetKey(KeyCode.Tab)){
+			animationTime += Time.deltaTime * animationSpeed;
+		}else{
+			animationTime -= Time.deltaTime * animationSpeed;
+		}
+		animationTime = Mathf.Clamp01(animationTime);
+
+		float t = animationTime;
+		float w = listRect.sizeDelta.x;
+		listRect.anchoredPosition = Vector2.left * (t * w - w);
+	}
+}
