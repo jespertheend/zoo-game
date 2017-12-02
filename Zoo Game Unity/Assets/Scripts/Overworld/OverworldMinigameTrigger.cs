d69b38219ee584a6875a03b996932f42b759a5c0
Jesper van den Ende <jespertheend@gmail.com>
2017-12-02 11:27:15 +0100

    Add triggers for entering scene in overworld

---
+++ Zoo Game Unity/Assets/Scripts/Overworld/OverworldMinigameTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverworldMinigameTrigger : MonoBehaviour {
+
+	public string sceneToStart;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		OverworldCharacter charScript = collider.gameObject.GetComponent<OverworldCharacter>();
+		if(charScript != null){
+			SceneManager.LoadScene(sceneToStart);
+		}
+	}
+}
