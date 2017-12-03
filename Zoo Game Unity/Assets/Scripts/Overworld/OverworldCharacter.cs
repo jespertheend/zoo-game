2505e7626b70a504798807c9690d0d05fe31add0
Jesper van den Ende <jespertheend@gmail.com>
2017-12-03 15:38:01 +0100

    add sprite to player in overworld

--- Zoo Game Unity/Assets/Scripts/Overworld/OverworldCharacter.cs
+++ Zoo Game Unity/Assets/Scripts/Overworld/OverworldCharacter.cs
@@ -6,6 +6,12 @@ public class OverworldCharacter : MonoBehaviour {
 
 	Rigidbody2D rb;
 
+	static bool hasLastWorldPosition;
+	static Vector3 lastWorldPosition;
+
+	public SpriteRenderer spriteRenderer;
+	public ChangeSprite changeSpriteScript;
+
 	public float maxWalkForce = 1f;
 	public float walkForceMultiplier = 1f;
 
@@ -13,10 +19,23 @@ public class OverworldCharacter : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D>();
 	}
 
+	void Start(){
+		if(hasLastWorldPosition){
+			transform.position = lastWorldPosition;
+		}
+	}
+
 	void Update() {
 		Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 		force *= walkForceMultiplier;
 		force = Vector2.ClampMagnitude(force, maxWalkForce);
 		rb.AddForce(force);
+		hasLastWorldPosition = true;
+		lastWorldPosition = transform.position;
+		bool walking = rb.velocity.magnitude > 0.1;
+		changeSpriteScript.enabled = walking;
+		if(walking){
+			spriteRenderer.flipX = rb.velocity.x > 0;
+		}
 	}
 }
